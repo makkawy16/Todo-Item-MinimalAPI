34005c93fa7a8b487fa96e94033c4142a8e3f395
makkawy16 <yousef.elmakkawy@gmail.com>
2025-09-19 22:48:28 +0300

    install in memory DB and create todo class

---
+++ Todo Item MinimalAPI/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Todo_Item_MinimalAPI
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
